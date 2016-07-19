57e11644028170e0550ee58fd1e18483ee6e3029
Aldo López <Aldo Lpez>
2016-07-19 16:09:56 -0500

    Add multiply function

--- ExampleGit.java
+++ ExampleGit.java
@@ -2,4 +2,10 @@ class ExampleGit{
 	public static void main(String... args){
 		System.out.println("Hello World");
 	}
+	public static int sum(int n1, int n2){
+		return n1 + n2;
+	}
+	public static int multiply(int n1, int n2){
+		return n1 * n2;
+	}
 }
